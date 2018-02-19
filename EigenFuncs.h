f59a797bbfce43836a95eb08dfb21a139213f243
hayatata <hayatata@live.jp>
2018-02-19 23:13:13 +0900

    プロジェクト ファイルを追加します。

---
+++ EigenFuncs.h
@@ -0,0 +1,44 @@
+#pragma once
+
+/*--------------*/
+/* EigenFuncs.h */
+/*--------------*/
+//#pragma once
+
+extern "C" {
+
+//識別子が定義されているか確認し、定義されていないなら、定義する
+/*
+#ifdef <識別子名>
+<処理>
+処理が複数行にわたる場合は、最後にendifを書く。
+
+*/
+/*
+#defineは、以下を行っている。文字列の置き換えである。
+機能 :文字列の置き換え
+書式 :#define <文字列1> <文字列2>
+*/
+/*
+__declspec(dllexport)は、エクスポート（DLLを作成）する際に必要。
+__declspec(dllimport)は、インポートする際に必要。別のアプリからこれを読んだときに使う？
+*/
+/*
+VisulaStudioにてコンパイルすす場合は、プリプロセッサに" EIGENFUNCS_EXPORTS"を設定する必要がある。
+*/
+#ifdef EIGENFUNCS_EXPORTS
+#define EIGEN_FUNCS_API __declspec(dllexport)
+#else //このelseはifdefに対するものだと考えられる。
+#define EIGEN_FUNCS_API __declspec(dllimport)
+#endif
+	EIGEN_FUNCS_API void InverseMat_FullPivLU(int dim, float a[], float ans[]);
+	/*
+	以下の一文は、インポートとエクスポート時に行頭がexportかimportに変化する。
+	これが、DLLで外に見える関数となる？
+	*/
+	
+/*
+キーワードを定義して、間接的にエクスポートする（？）メリットは TEST_EXPORTSの定義されていないプロジェクトで このヘッダーがインクルードされたときに明らかになります。 
+そのとき、_EXPORTイコール__declspec(dllimport)となり、 関数はインポートされるからです。
+*/
+} // extern "C"
